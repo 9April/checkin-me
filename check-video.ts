94eca8703e4132c2703f439d95910e7ee6f21dbe
9April <pedrooussama@gmail.com>
2026-08-17 16:37:17 +0100

    Remove debug scratch scripts breaking the build

--- check-video.ts
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-async function main() {
-  const property = await prisma.property.findUnique({ where: { slug: 'khouzama07' } })
-  console.log('Video URL:', property?.mediaVideoUrl)
-}
-main().finally(() => prisma.$disconnect())
